963e278bfb83ffc544e5184e9dd422329ea90b4b
TeoVala <theodorvalavanis@gmail.com>
2022-11-15 09:14:55 +0200

    Update Config.php
    * removed api key

--- Classes/Config.php
+++ Classes/Config.php
@@ -6,9 +6,9 @@ class Config {
 
     const SMTP_PORT = 2525;
 
-    const SMTP_USER = 'c3947c16e6b226';
+    const SMTP_USER = '';
 
-    const SMTP_PASS = '8660ec9a9ac16a';
+    const SMTP_PASS = '';
 
 
 
